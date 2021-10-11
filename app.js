1159f3fb9e6cec5d3e52827b043dc8147edc1a2c
Mustafa Kahraman <80291704+kahramanX@users.noreply.github.com>
2021-10-11 18:06:03 +0300

    added my code for test

---
+++ app.js
@@ -0,0 +1,25 @@
+// EXPRESS 
+let express = require('express');
+let path = require('path');
+let fs = require("fs"); 
+let app = express();
+
+// file require
+let routeElektronik = require(path.join(__dirname, "./app_server/routes/ElektronikRoute.js"));
+// bazen bunu kullanmak gerekiyor => (path.join(__dirname, " ")
+
+const port = 8080;
+const hostname = "127.0.0.1";
+
+//bunu yapmazsan public içindeki css dosyalarını göremez. Bu kod static, dosyaları kullanıcıya görünür yapar
+app.use('/public', express.static(path.join(__dirname, 'public')));
+//__dirname çağrıldığı klasörü getiriyor
+
+app.use("/elektronik", routeElektronik);
+
+
+app.listen(port, hostname, ()=>{
+    console.log(`server çalışıyooo http://localhost:${port}`);
+
+});
+
